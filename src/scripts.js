942944d847515935d9188c0c6067f7188470600e
Christopher Campbell <christophercam@gmail.com>
2017-01-14 20:51:41 +0100

    add year of birth

--- src/scripts.js
+++ src/scripts.js
@@ -1,30 +1,50 @@
 import Rx from 'rxjs/Rx';
 
 const firstName = document.querySelector('#first-name');
-const secondName = document.querySelector('#second-name');
+const lastName = document.querySelector('#last-name');
 const fullName = document.querySelector('#full-name');
+const birthYear = document.querySelector('#select-birth-year');
+const birthYearAgo = document.querySelector('#birth-years-ago');
 
-let firstNameVal = new Rx.BehaviorSubject('');
-let secondNameVal = new Rx.BehaviorSubject('');
+let firstNameVal = new Rx.BehaviorSubject('first');
+let lastNameVal = new Rx.BehaviorSubject('last');
+let fullNameVal = new Rx.BehaviorSubject('first last');
+let birthYearVal = new Rx.BehaviorSubject(2000);
+let ageVal = new Rx.BehaviorSubject(0);
 
-const changeNameFn = (subject) => (value) => subject.next(value);
+const nextFn = (subject) => (value) => subject.next(value);
 const errorFn = (err) => console.error(err);
 
-const firstNameChange = changeNameFn(firstNameVal);
-const secondNameChange = changeNameFn(secondNameVal);
+const firstNameChange = nextFn(firstNameVal);
+const lastNameChange = nextFn(lastNameVal);
+const birthYearChange = nextFn(birthYearVal);
 
 firstNameVal.subscribe((value) => {
-    fullName.innerHTML = `${value} ${secondNameVal.value}`;
+    fullNameVal.next(`${value} ${lastNameVal.value}`);
 });
 
-secondNameVal.subscribe((value) => {
-    fullName.innerHTML = `${firstNameVal.value} ${value}`;
+lastNameVal.subscribe((value) => {
+    fullNameVal.next(`${firstNameVal.value} ${value}`);
 });
 
-const changeField = (field, fieldNameChangeFn) =>
+birthYearVal.subscribe((value) => {
+    var year = new Date().getFullYear();
+    ageVal.next(year - value);
+});
+
+const changeSelect = (field, fieldNameChangeFn) =>
+    Rx.Observable.fromEvent(field, 'change')
+        .pluck('target', 'value')
+        .subscribe (fieldNameChangeFn, errorFn);
+
+const changeInput = (field, fieldNameChangeFn) =>
     Rx.Observable.fromEvent(field, 'keyup')
         .pluck('target', 'value')
         .subscribe (fieldNameChangeFn, errorFn);
 
-changeField(firstName, firstNameChange);
-changeField(secondName, secondNameChange);
+changeInput(firstName, firstNameChange);
+changeInput(lastName, lastNameChange);
+changeSelect(birthYear, birthYearChange);
+
+fullNameVal.subscribe((val) => fullName.innerHTML = val);
+ageVal.subscribe((val) => birthYearAgo.innerHTML = val);
