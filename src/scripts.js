509767292cadd033a70d4e9a6d226fd6ad4ad4d4
Christopher Campbell <christophercam@gmail.com>
2017-01-15 10:09:34 +0100

    add hobby textarea with data-binding

--- src/scripts.js
+++ src/scripts.js
@@ -5,19 +5,24 @@ const lastName = document.querySelector('#last-name');
 const fullName = document.querySelector('#full-name');
 const birthYear = document.querySelector('#select-birth-year');
 const birthYearAgo = document.querySelector('#birth-years-ago');
+const hobbieInput = document.querySelector('#hobbies');
+const hobbyOutput = document.querySelector('#hobbies-output');
 
 let firstNameVal = new Rx.BehaviorSubject('first');
 let lastNameVal = new Rx.BehaviorSubject('last');
 let fullNameVal = new Rx.BehaviorSubject('first last');
 let birthYearVal = new Rx.BehaviorSubject(2000);
 let ageVal = new Rx.BehaviorSubject(0);
+let hobbies = new Rx.BehaviorSubject([]);
 
 const nextFn = (subject) => (value) => subject.next(value);
+const nextArrayFn = (subject) => (value) => subject.next(value.split(" "));
 const errorFn = (err) => console.error(err);
 
 const firstNameChange = nextFn(firstNameVal);
 const lastNameChange = nextFn(lastNameVal);
 const birthYearChange = nextFn(birthYearVal);
+const hobbyChange = nextArrayFn(hobbies);
 
 firstNameVal.subscribe((value) => {
     fullNameVal.next(`${value} ${lastNameVal.value}`);
@@ -28,8 +33,7 @@ lastNameVal.subscribe((value) => {
 });
 
 birthYearVal.subscribe((value) => {
-    var year = new Date().getFullYear();
-    ageVal.next(year - value);
+    ageVal.next(new Date().getFullYear() - value);
 });
 
 const changeSelect = (field, fieldNameChangeFn) =>
@@ -45,6 +49,13 @@ const changeInput = (field, fieldNameChangeFn) =>
 changeInput(firstName, firstNameChange);
 changeInput(lastName, lastNameChange);
 changeSelect(birthYear, birthYearChange);
+changeInput(hobbieInput, hobbyChange);
 
 fullNameVal.subscribe((val) => fullName.innerHTML = val);
 ageVal.subscribe((val) => birthYearAgo.innerHTML = val);
+hobbies.subscribe((tags) => {
+    hobbyOutput.innerHTML = tags
+        .filter((tag) => tag !== '')
+        .map((tag) => `<div class="tag">${tag}</div>`)
+        .join('');
+});
